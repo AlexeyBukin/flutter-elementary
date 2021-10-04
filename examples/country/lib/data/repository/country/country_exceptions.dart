2ba89477eb9b8350eec1511d2303e325194d9e1b
Vyacheslav Pak <VVPAK14@gmail.com>
2021-10-04 21:05:14 +0600

    added error

--- examples/country/lib/data/repository/country/country_exceptions.dart
+++ examples/country/lib/data/repository/country/country_exceptions.dart
@@ -2,4 +2,9 @@ class GetAllCountriesException implements Exception {
   final String description;
 
   GetAllCountriesException(this.description);
+
+  @override
+  String toString() {
+    return 'Error while getting countries: $description';
+  }
 }
